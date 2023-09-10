108db1c75653a431a917283069155e2de714afa1
SelvamKrishna <126485131+SelvamKrishna@users.noreply.github.com>
2023-09-11 01:01:16 +0800

    Add files via upload

---
+++ Coffee Machine/Main.py
@@ -0,0 +1,163 @@
+from Data import DATASET
+
+LINE: str = "-" * 50
+
+class Ingredient:
+    def __init__(self, quantity: int) -> None:
+        self.quantity = quantity
+        
+    def CheckAvailable(self, amount: int) -> bool:
+        return True if amount <= self.quantity else False
+    
+    def Consume(self, amount: int):
+        self.quantity -= amount
+        
+class Coins:
+    def CalculateChange(self, amount: float, price: float) -> list:
+        self.coins: list[float] = [1.0, 0.5, 0.2, 0.1]
+        change: float = amount - price
+        change = round(change, 1)
+        
+        self.changeSet = []
+        
+        while round(sum(self.changeSet), 1) != change:
+            self.changeSet.append(self.coins[3])       
+        
+        self.tenCent = self.changeSet.count(self.coins[-1])
+        isMicroprocessing = True
+        
+        while isMicroprocessing:
+            if self.tenCent >= 10:
+                self.MicroProcess(10, 0)
+            elif self.tenCent >= 5:
+                self.MicroProcess(5, 1)
+            elif self.tenCent >= 2:
+                self.MicroProcess(2, 2)
+            else:
+                isMicroprocessing = False
+                    
+        return self.changeSet
+
+    def MicroProcess(self, size: int, coin: int):
+        for _ in range(size):
+            self.changeSet.remove(self.coins[3])
+        self.tenCent -= size
+        self.changeSet.append(self.coins[coin])
+        
+class Coffee:
+    def __init__(self, id: int) -> None:
+        self.id: int = id
+        detail: dict = DATASET[self.id]
+        
+        self.name: str = detail["name"]
+        self.price: float = detail["price"]
+        self.waterAmt: int = detail["process"][0]
+        self.milkAmt: int = detail["process"][1]
+        self.syrup: int = detail["process"][2]
+        
+    def DisplayDetails(self):
+        print(f"Name     : {self.name}")
+        print(f"Price    : {self.price}")
+        
+        
+class Machine:
+    def __init__(self) -> None:
+        self.water = Ingredient(quantity = 2500)
+        self.milk = Ingredient(quantity = 300)
+        self.syrup = Ingredient(quantity = 1500)
+        
+        self.coins = Coins()
+        
+        self.orders: list[str] = []
+        
+        for idx in DATASET:
+            self.orders.append(str(idx['id']))
+        
+        self.isOperating: bool = True
+        self.adminComms: list[str] = ["report", "off"]
+        
+        self.GetInputs()
+        
+    def GetInputs(self):
+        while self.isOperating:
+            print(LINE)
+            print("WELCOME TO COFFEE PALACE!!")
+            print(LINE)
+            print("What would you like to order?")
+            for idx in DATASET:
+                print(f"{idx['id']} : {idx['name']}")
+            
+            userIn = input(": ").lower()
+            if userIn in self.adminComms:
+                self.ControlPanel(command = userIn)
+            
+            elif userIn in self.orders:
+                self.ProductOrder(int(userIn))
+            
+            else:
+                print("\nERROR : Invalid Input!!\n")
+            
+        print(LINE)
+        print(f"Machine Operating : {self.isOperating}")
+        print(LINE)
+            
+    def ControlPanel(self, command: str):
+        if command == "report":
+            print(LINE)
+            print(f"Machine Operating : {self.isOperating}")
+            print(f"Water Quantity    : {self.water.quantity}")
+            print(f"Milk Quantity     : {self.milk.quantity}")
+            print(f"Syrup Quantity    : {self.syrup.quantity}")
+        if command == "off":
+            self.isOperating = False
+            
+    def ProductOrder(self, id: int):
+        coffee = Coffee(id)
+        canProcess: bool = False
+        
+        if coffee.waterAmt <= self.water.quantity:
+            if coffee.milkAmt <= self.milk.quantity:
+                if coffee.syrup <= self.syrup.quantity:
+                    canProcess = True
+        
+        print(LINE)
+        if canProcess is True:    
+            coffee.DisplayDetails()
+            print(LINE)
+            print("Input Payment to Confirm Order (x) to Cancel : ")
+            
+            payAmount: list[float] = [0]
+            confirmOrder: bool = True
+            
+            while sum(payAmount) < coffee.price and confirmOrder is True:
+                print(f"Paid : {sum(payAmount)}")
+                payIn = input("Insert Coins (1.0, 0.5, 0.2, 0.1) :").lower()
+                if payIn == 'x':
+                    confirmOrder = False
+                    break
+                elif payIn in ("1.0", "0.5", "0.2", "0.1"):
+                    payAmount.append(float(payIn))
+                else: pass
+            
+            if confirmOrder is True:
+                print(LINE)
+                payment: float = sum(payAmount)
+                
+                change = self.coins.CalculateChange(payment, coffee.price)
+                print(f"Payment : {round(payment, 2)}")
+                print(f"Price   : {round(coffee.price, 2)}")
+                print(f"Change  : {round(sum(change), 2)}")
+                print(f"Given   : {change}")
+                
+                self.water.Consume(coffee.waterAmt)
+                self.milk.Consume(coffee.milkAmt)
+                self.syrup.Consume(coffee.syrup)
+                
+                print(LINE)
+                print("Thank You For Your Purchase!!\nEnjoy your Coffee!!")
+            
+        else:
+            print("Sorry Product out of Stock!!")
+        
+            
+machine = Machine()
